3424a042b1859712359e39e28458e3c3060f94c8
digvijay-gavas <43606865+digvijay-gavas@users.noreply.github.com>
2018-10-01 15:38:22 +0530

    Create TestApp2.java

---
+++ src/test/java/com/mycompany/app/TestApp2.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import junit.framework.TestCase;
+
+public class TestApp2 extends TestCase {
+
+	public void testApp()
+    {
+    	
+    	assertEquals(App.getHelloWorld2(), "Hello World 2");
+ 
+    }
+}
