d142ab9115394ee9bd140b1eaa65a09a5fd0fc7f
digvijay-gavas <43606865+digvijay-gavas@users.noreply.github.com>
2018-10-03 18:25:54 +0530

    Update SeleniumTest.java

--- src/test/java/com/mycompany/app/SeleniumTest.java
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -6,12 +6,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import junit.framework.TestCase;
 
@@ -24,7 +27,7 @@ public class SeleniumTest extends TestCase {
  	protected void setUp() throws Exception {
  		// TODO Auto-generated method stub
  		
- 		String service = "C:\\Knowlege_sharing_sessions\\tools\\IEDriverServer.exe";
+ 		String service = "D:\\knowlege_sharing\\tools\\IEDriverServer.exe";
 		System.setProperty("webdriver.ie.driver", service);
 		
 		
@@ -35,8 +38,8 @@ public class SeleniumTest extends TestCase {
 		
 		
 		  _driver = new InternetExplorerDriver(capabilities);
-		  _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- 
+		  _driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		  
 		
  		
  		//System.setProperty("webdriver.firefox.driver","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
@@ -46,22 +49,33 @@ public class SeleniumTest extends TestCase {
  		//_driver = new FirefoxDriver(firefoxProfile);
  		System.out.println("------++++1");
  	}
+ 	public void waitForPageLoadComplete(WebDriver driver, int specifiedTimeout) {
+ 		
+ 		System.out.println("---waiting "+ driver.getCurrentUrl());
+ 	    Wait<WebDriver> wait = new WebDriverWait(driver, specifiedTimeout);
+ 	    wait.until(driver1 -> String
+ 	            .valueOf(((JavascriptExecutor) driver1).executeScript("return document.readyState"))
+ 	            .equals("complete"));
+ 	    ;
+ 	   System.out.println("---loaded "+ driver.getCurrentUrl());
+ 	}
+ 	
  	
  	@Test
 	public void testApp()
     {
- 		System.out.println("------++++2");
- 		_driver.get("https://www.google.co.in/");
- 		System.out.println("------++++3");
- 		assertEquals("Google", _driver.getTitle());
- 		System.out.println("------++++4");
- 		_driver.findElement(By.id("lst-ib")).clear();
  		
- 	
+ 		_driver.get("https://www.google.co.in/");
+ 		waitForPageLoadComplete(_driver, 10);
+ 				
+ 		assertEquals("Google", _driver.getTitle());
+ 		
+ 		_driver.findElement(By.id("lst-ib")).clear();
  		_driver.findElement(By.id("lst-ib")).sendKeys("selenium\n");
-
  		//_driver.findElement(By.name("btnK")).click();
+ 		waitForPageLoadComplete(_driver, 10);
  		_driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+ 		waitForPageLoadComplete(_driver, 10);
  		//assertEquals(" ", _driver.getTitle());
  		assertEquals("Selenium - Web Browser Automation", _driver.getTitle());
     }
